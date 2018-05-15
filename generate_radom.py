e3c1e48dfaf9730c8b29a4be448032aa5869fea4
maddy46 <39220141+maddy46@users.noreply.github.com>
2018-05-15 22:44:07 +0530

    adding file

---
+++ generate_radom.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+weight = np.random.random((4,3))
+bias = np.random.random((1,3))
+wout = np.random.random((3,1))
+bout = np.random.random((1,1))
+print weight
+print bias
+print wout
+print bout
